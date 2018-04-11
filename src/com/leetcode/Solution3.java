c2a8bde5cf217be72b8db0203f00bca5bcbc5356
hisachyikwok <hsiachyikwok@gmail.com>
2018-04-11 21:38:54 +0800

    使用hashset解决重复问题。o(n)

--- src/com/leetcode/Solution3.java
+++ src/com/leetcode/Solution3.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.HashSet;
+
 /**
  * @ 是否包含重复数字
  */
@@ -17,7 +19,7 @@ public class Solution3 {
     }
 
 
-    //快速排序
+    //快速排序(降序)
     private void quickSort(int left, int right, int[] nums) {
         int i = left;
         int j = right;
@@ -39,5 +41,28 @@ public class Solution3 {
             quickSort(left, j - 1, nums);
             quickSort(i + 1, right, nums);
         }
+    }
+
+    //暴力破解
+    public boolean containsDuplicate1(int[] nums) {
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (j != i && nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) { //使用HashSet
+
+        HashSet<Integer> set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
